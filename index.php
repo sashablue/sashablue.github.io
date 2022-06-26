f13aa39f7eeb7eac25570d5ea5b31276479a0461
Sasha Blue <mailsashablue@gmail.com>
2022-06-26 21:24:01 +0300

    Sun 26 Jun 2022 09:24:01 PM EEST

---
+++ index.php
@@ -0,0 +1,31 @@
+2022-06-26 21:20:01 EEST (+0300)
+<br>
+up 2 days, 22:34,  2 users,  load average: 0.05, 0.03, 0.00
+<br>
+<br>
+Local IP icanhazip.com:
+82.79.173.40
+<br>
+Local IP wtfismyip.com/text:
+82.79.173.40
+<br>
+Local IP ifconfig.me:
+82.79.173.40<br>
+<br>
+<script src="scripts/main.js"></script>
+<span id="ipaddress"></span>
+<br>
+<pre><span class="inner-pre" style="font-size: 14px">
+Filesystem      Size  Used Avail Use% Mounted on
+/dev/sda1        25G   14G  9.7G  59% /
+/dev/sdc1       1.8T  529G  1.2T  31% /home/StorageB
+/dev/sdb1       1.8T  529G  1.2T  31% /home/StorageA
+---
+Package id 0:  +42.0°C  (high = +85.0°C, crit = +105.0°C)
+Core 0:        +42.0°C  (high = +85.0°C, crit = +105.0°C)
+Core 1:        +37.0°C  (high = +85.0°C, crit = +105.0°C)
+---
+/dev/sda: SAMSUNG HD251HJ: 38°C
+/dev/sdb: ST2000VN004-2E4164: 33°C
+/dev/sdc: ST2000VN004-2E4164: 34°C
+</span></pre>
